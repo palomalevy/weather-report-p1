f4fa0dd3ce439b59ad24e786b6ea193636efe437
Paloma Levy <palomalevy@gmail.com>
2025-06-17 10:08:44 -0700

    add helmet

--- src/Day.jsx
+++ src/Day.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Day.css'
 
-function Day({ date, temperature, weather }) {
+function Day(props) {
+
+  const { date, temperature, weather } = props
 
   const temperatureClass = temperature < 60 ? "cold" : "hot";
-  const weatherEmoji = {"Windy": "🍃", "Sunny": "☀️", "Cloudy":"☁️", Rainy:"☔️"}
+  const weatherEmoji = {"Windy": "🍃", "Sunny": "☀️", "Cloudy": "☁️", Rainy:"☔️"}
+  const theClass = temperature < 60 ? "cold" : "hot"
+
+  debugger
 
   return (
     <div>
       <h2>{date}</h2>
       <p className="weather">{weatherEmoji[weather]}</p>
-      <p className={`temperature ${temperatureClass}`}>{temperature}°</p>
+      <p className={`temperature ${temperature < 60 ? "cold" : "hot"}`}>{temperature}°</p>
     </div>
   )
 }
